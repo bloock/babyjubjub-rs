a11c2b40da32597f09cf9e279ab486fd560b6d46
marcbaque <marc@bloock.com>
2023-10-24 10:28:17 +0200

    update package name

--- benches/bench_babyjubjub.rs
+++ benches/bench_babyjubjub.rs
@@ -1,23 +1,22 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-extern crate rand;
-#[macro_use]
 extern crate ff;
+extern crate rand;
 use ff::*;
 
 extern crate num;
 extern crate num_bigint;
 use num_bigint::{BigInt, ToBigInt};
 
-use babyjubjub_rs::Point;
+use bloock_babyjubjub_rs::Point;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let p: Point = Point {
-        x: babyjubjub_rs::Fr::from_str(
+        x: bloock_babyjubjub_rs::Fr::from_str(
             "17777552123799933955779906779655732241715742912184938656739573121738514868268",
         )
         .unwrap(),
-        y: babyjubjub_rs::Fr::from_str(
+        y: bloock_babyjubjub_rs::Fr::from_str(
             "2626589144620713026669568689430873010625803728049924121243784502389097019475",
         )
         .unwrap(),
@@ -40,16 +39,16 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("point compress", |b| b.iter(|| p.compress()));
     let p_comp = p.compress();
     c.bench_function("point decompress", |b| {
-        b.iter(|| babyjubjub_rs::decompress_point(p_comp))
+        b.iter(|| bloock_babyjubjub_rs::decompress_point(p_comp))
     });
 
-    let sk = babyjubjub_rs::new_key();
+    let sk = bloock_babyjubjub_rs::new_key();
     let pk = sk.public();
     let msg = 5.to_bigint().unwrap();
     c.bench_function("sign", |b| b.iter(|| sk.sign(msg.clone())));
     let sig = sk.sign(msg.clone()).unwrap();
     c.bench_function("verify", |b| {
-        b.iter(|| babyjubjub_rs::verify(pk.clone(), sig.clone(), msg.clone()))
+        b.iter(|| bloock_babyjubjub_rs::verify(pk.clone(), sig.clone(), msg.clone()))
     });
 }
 
